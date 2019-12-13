e549474d20feaa25c388b6abd05434c9837c9b1b
ElAntCar <58670592+ElAntCar@users.noreply.github.com>
2019-12-13 16:02:18 +0100

    Update Centric X Pre-Beta.lua

--- Centric X Pre-Beta.lua
+++ Centric X Pre-Beta.lua
@@ -64,7 +64,7 @@ Version.BorderSizePixel = 0
 Version.Position = UDim2.new(0.453324966, 0, 0.669510841, 0)
 Version.Size = UDim2.new(0, 200, 0, 50)
 Version.Font = Enum.Font.SourceSansBold
-Version.Text = "Version: 0.0.9-1 (Pre-Beta)"
+Version.Text = "Version: 0.0.9-2 (Pre-Beta)"
 Version.TextColor3 = Color3.new(0.0862745, 0.0862745, 0.0862745)
 Version.TextScaled = true
 Version.TextSize = 14
@@ -436,7 +436,7 @@ ChatBypass.BorderSizePixel = 0
 ChatBypass.Position = UDim2.new(0.249208622, 0, 0.423, 0)
 ChatBypass.Size = UDim2.new(0, 200, 0, 50)
 ChatBypass.Font = Enum.Font.SourceSansBold
-ChatBypass.Text = "[FE] Kill"
+ChatBypass.Text = "Chat Bypass"
 ChatBypass.TextColor3 = Color3.new(0, 0, 0)
 ChatBypass.TextSize = 32
 ChatBypass.TextWrapped = true
@@ -869,7 +869,7 @@ MainFrame.Visible = true
 wait(0.3)
 
 game.StarterGui:SetCore("SendNotification", {
-    Title = "Centric X 0.0.9-1 Loaded";
+    Title = "Centric X 0.0.9-2 Loaded";
     Text = "GUI made by AntCar#3763 (not scripts)";
 })
 wait(6.5)
